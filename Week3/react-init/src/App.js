81ea5f2953a23064a9e8920f0f9c757e94769281
Aleksandar Vrhovac <avrhovac92@gmail.com>
2018-02-23 21:04:26 +0100

    Added linter

--- Week3/react-init/src/App.js
+++ Week3/react-init/src/App.js
@@ -3,7 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'lets try some random text'
+    };
+  }
   render() {
+    const { state: { text }, changeState } = this;
     return (
       <div className="App">
         <header className="App-header">
@@ -12,10 +19,22 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <br />
+          lets try this <br />
         </p>
+        <div onClick={changeState}>{text}</div>
       </div>
     );
   }
+
+  changeState = () => {
+    this.setState(() => ({
+      text:
+        this.state.text === 'Changed text'
+          ? 'lets try some random text'
+          : 'Changed text'
+    }));
+  };
 }
- 
+
 export default App;

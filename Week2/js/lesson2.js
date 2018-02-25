2051ded2e2158324cdad2035b2b4c265ac017dad
Aleksandar Vrhovac <avrhovac92@gmail.com>
2018-02-25 10:25:27 +0100

    Initialised React application

--- Week2/js/lesson2.js
+++ Week2/js/lesson2.js
@@ -2,31 +2,30 @@
 Data type: boolean
 */
 
-var age = prompt("Koliko imate godina?");
-var height = prompt("Koliko ste visoki?");
+var age = prompt('Koliko imate godina?');
+var height = prompt('Koliko ste visoki?');
 var conditionOne = age < 16;
 var conditionTwo = parseInt(age) === 16;
 
-if(conditionOne){
-	console.log('Vi imate manje godina od 16.');
-} else if(conditionTwo){
-	console.log("Vi imate 16 godina.");
+if (conditionOne) {
+  console.log('Vi imate manje godina od 16.');
+} else if (conditionTwo) {
+  console.log('Vi imate 16 godina.');
 } else {
-	console.log("Vi imate vise od 16 godina.");
+  console.log('Vi imate vise od 16 godina.');
 }
 
-if(age > 18 && height < 150){
-	console.log("Vi ste niski u odnosu na godine.");
-} else if (age < 18 && height > 180){
-	console.log("Vi ste visoki u odnosu na godine.");
+if (age > 18 && height < 150) {
+  console.log('Vi ste niski u odnosu na godine.');
+} else if (age < 18 && height > 180) {
+  console.log('Vi ste visoki u odnosu na godine.');
 } else {
-	console.log("Vi ste normalni.");
+  console.log('Vi ste normalni.');
 }
 
-if(height < 130 || height > 200){
-	console.log("Vi niste prosjek.");
+if (height < 130 || height > 200) {
+  console.log('Vi niste prosjek.');
 }
-
 
 /*
  if condition example:
--- Week3/react-init/src/App.js
+++ Week3/react-init/src/App.js
@@ -3,14 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: 'lets try some random text'
-    };
-  }
   render() {
-    const { state: { text }, changeState } = this;
     return (
       <div className="App">
         <header className="App-header">
@@ -22,19 +15,9 @@ class App extends Component {
           <br />
           lets try this <br />
         </p>
-        <div onClick={changeState}>{text}</div>
       </div>
     );
   }
-
-  changeState = () => {
-    this.setState(() => ({
-      text:
-        this.state.text === 'Changed text'
-          ? 'lets try some random text'
-          : 'Changed text'
-    }));
-  };
 }
 
 export default App;
